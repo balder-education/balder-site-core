779df984b8977c5ad13b839d4f3cb979c175ca7c
Rogério Fontes Tomaz <rogerio@wd2s.com.br>
2015-11-25 22:28:26 -0200

    init0-structure

---
+++ src/main/java/br/com/baldereducation/sitecore/SitecoreApplication.java
@@ -0,0 +1,12 @@
+package br.com.baldereducation.sitecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitecoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SitecoreApplication.class, args);
+    }
+}
